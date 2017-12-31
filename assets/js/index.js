7a0330e29799da7d9d11eb7d2136ea67214a9774
Darxre <cakir.ilker329@gmail.com>
2018-01-01 01:12:25 +0300

    Moved js code to seperate files.

---
+++ assets/js/index.js
@@ -0,0 +1,23 @@
+var skills = ["skiing", "fencing", "reading about history", "design"],
+    counter = skills.length - 1,
+    previousSkill = $(".hobbies"),
+    arraylength = skills.length - 1;
+
+function display_skills() {
+    if (counter === arraylength) {
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+    $('.hobbies > span > strong').fadeOut(200, function () {
+        $(this).text(skills[counter]).fadeIn(200);
+    });
+
+}
+
+display_skills();
+
+setInterval(function () {
+    display_skills();
+}, 2500);
